9c7855c2d047ea3d126eb694df937c85b811ed1a
Haugholt <haugholt@gmail.com>
2011-11-25 14:53:26 +0100

    Added ignore file for visual studio and added remaining files.

--- EgoSKB2QIF/SKB2QIF/Program.cs
+++ EgoSKB2QIF/SKB2QIF/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SKB2QIF.Core;
 
 namespace SKB2QIF
 {
@@ -9,6 +10,27 @@ namespace SKB2QIF
     {
         static void Main(string[] args)
         {
+            FileReader fr = new FileReader(args[0]);
+            var lines = fr.ReadAllLines();
+
+            int i = 0;
+            lines.ForEach(a => Console.Out.WriteLine("{0}: {1}", i++,a));
+
+            Console.Out.WriteLine("\nSimpleSplit:\n");
+
+            lines.ForEach(l => Parse(l));
+        }
+
+        private static void Parse(string l)
+        {
+            var sl= l.Split(new char[]{'\t'});
+
+            for (int i = 0; i < sl.Length; i++)
+            {
+                Console.Out.WriteLine("{0}: {1}", i, sl[i]);
+            }
+
+            Console.Out.WriteLine();
         }
     }
 }
