1963c86dbdb3ce8cc4118d3d995e6a0b5325f6af
Haugholt <haugholt@gmail.com>
2011-11-25 14:35:41 +0100

    Simple fileread and split into lines and tab-separators.

---
+++ EgoSKB2QIF/SKB2QIF/Core/FileReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SKB2QIF.Core
+{
+    public class FileReader
+    {
+        public FileReader(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public string FileName { get; private set; }
+
+        public List<string> ReadAllLines(){
+            return this.ReadAllLines(FileName);
+        }
+
+        private List<string> ReadAllLines(string fileName)
+        {
+            System.IO.FileInfo fifo = new FileInfo(fileName);
+
+            List<string> lines = new List<string>();
+            if (!fifo.Exists) return lines;
+
+            using (StreamReader rea = new StreamReader(fileName, true))
+            {
+                while (!rea.EndOfStream)
+                {
+                    lines.Add(
+                        rea.ReadLine());
+                }
+            }
+            return lines;
+        }
+    }
+
+}
